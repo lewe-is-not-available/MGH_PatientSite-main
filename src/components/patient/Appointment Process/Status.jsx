c5e120a54d9f105aa709c205e47fdcf0c13a5e0a
John Doe <loewiayon12@gmail.com>
2023-11-01 15:00:37 +0800

    latest

--- src/components/patient/Appointment Process/Status.jsx
+++ src/components/patient/Appointment Process/Status.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import supabase from '../../config/Supabase'
 
-useEffect(() => {
-  const fetchFilter = async () => {
-    const { data, error } = await supabase.from("Patient_Appointments").select("*");
-    if (error) {
-      console.error("Failed to fetch", error.message);
-    } else {
-      const nameSuggest = data.filter((doctor) =>
-        doctor.Name.toLowerCase().includes(Name.toLowerCase())
-      );
-      setFilter(nameSuggest);
-      if (Name === "") {
-        setFilter("");
-      }
-    }
-  };
-  fetchFilter();
-}, [Name]);
 
 const Status = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchFilter = async () => {
+      const { data, error } = await supabase.from("Patient_Appointments").select("*");
+      if (error) {
+        console.error("Failed to fetch", error.message);
+      }
+      setData(data)
+    };
+    fetchFilter();
+  }, []);
+  
   return (
-    <div className='back h-screen w-full flex place-content-center'><div className='abs absolute mt-10 bg-white p-10'>lkjlkj</div></div>
+    <div className='back h-screen w-full flex place-content-center'><div className='abs absolute mt-10 bg-white p-10'>{}</div></div>
   )
 }
 
